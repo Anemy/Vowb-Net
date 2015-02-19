38bcf21f7d0a2476c615fe2e6d1587fb44f2f462
Mason Everett <everetm@purdue.edu>
2015-02-19 18:17:45 -0500

    added a horrible login page popup that I need to format

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -2,5 +2,14 @@
 
 var loginButtonClicked = function() {
     /* Do something */
+$("#modal_trigger").leanModal({top : 200, overlay : 0.6, closeButton: ".modal_close" });
+$(function(){
+  // Calling Login Form
+  $("#login_form").click(function(){
+    console.log("derp");
+    $(".user_login").show();
+    return false;
+  });
+})
     console.log("Login button clicked.");
 }
