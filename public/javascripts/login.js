05269205236387a8eb4ce1f7b7d0360df167aaa8
Mason Everett <everetm@purdue.edu>
2015-02-20 23:59:59 -0500

    working login popup with overlay

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,15 +1,23 @@
 /* This script will open the log in dialogue and handle client side log in things */
 
+
 var loginButtonClicked = function() {
     /* Do something */
-$("#modal_trigger").leanModal({top : 200, overlay : 0.6, closeButton: ".modal_close" });
-$(function(){
-  // Calling Login Form
-  $("#login_form").click(function(){
+    $('.loginPage').fadeIn(600);
+    //$('.mask').fadeIn(500);
+    //$('.loginPopup').fadeIn(700);
     console.log("derp");
-    $(".user_login").show();
-    return false;
-  });
-})
-    console.log("Login button clicked.");
 }
+  // Calling Login Form
+//$(function(){//  $(".user_login").show();
+//    console.log("Login button clicked.");
+//    console.log("derp");
+//});
+
+//$(document).ready( function() {
+//  $("#trigger").leanModal({top : 200, overlay : .6, closeButton: ".modal_close" });
+
+//  $("#trigger").click( function() {
+//    $(".user_login").show();
+//  });
+//});
