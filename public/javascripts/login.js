93623023099f284eb0b069c37951b798949d5271
Rhys Howell <rhysh@live.com>
2015-02-23 15:19:47 -0500

    Sign up page added

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,15 +1,9 @@
 /* This script will open the log in dialogue and handle client side log in things */
 
 var loginButtonClicked = function() {
-    /* Do something */
-$("#modal_trigger").leanModal({top : 200, overlay : 0.6, closeButton: ".modal_close" });
-$(function(){
-  // Calling Login Form
-  $("#login_form").click(function(){
-    console.log("derp");
-    $(".user_login").show();
-    return false;
-  });
-})
+    //make stuff appear
+    $('.blackBackground').fadeIn(500);
+    $('.loginSomething').fadeIn(500);
+
     console.log("Login button clicked.");
 }
--- public/javascripts/signupPage.js
+++ public/javascripts/signupPage.js
@@ -6,6 +6,8 @@ $('#signupForm').ready (function() {
     $('#signupForm').submit( function (event) {
         console.log ("Signup form submitted");
 
+        // get data from each thing and send to server
+
         // return false stops the page from reloading
         return false;
     });
