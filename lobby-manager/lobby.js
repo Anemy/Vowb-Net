57283728b4eb5402c0a67a62d9a42463a17e36e6
iyer8 <iyer8@purdue.edu>
2015-02-24 17:28:43 -0500

    Can print messages to screen in text chat

--- lobby-manager/lobby.js
+++ lobby-manager/lobby.js
@@ -8,10 +8,19 @@ var lobbyManager = module.exports =  {
 lobbyManager.startListening = function(http) {
     this.io = require('socket.io')(http);
     //require('http').Server(app);
+    
+    //hacky fix for stuff
+    var that = this;
 
     this.io.on('connection', function(socket) {
-        console.log("New socket io connection made.");
+      console.log("New socket io connection made.");
         // can do something with socket here.
+
+	  socket.on('chat message', function(msg){
+	  	console.log("Message Received");
+	    that.io.emit('chat message', msg);
+	  });
 
     });
 }
+
