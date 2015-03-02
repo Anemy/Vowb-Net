ed03d6aa773dd197e694f6e4b29ad82fd6097079
iyer8 <iyer8@purdue.edu>
2015-03-02 18:20:10 -0500

    Clean up

--- lobby-manager/lobby.js
+++ lobby-manager/lobby.js
@@ -40,30 +40,5 @@ lobbyManager.startListening = function(http) {
         that.io.emit('chat message', 'A user has disconnected.');
       });
 
-      // socket.on('message', function (message) {
-      //     log('Got message:', message);
-      // // for a real app, would be room only (not broadcast)
-      //     socket.broadcast.emit('message', message);
-      // });
-      //
-      // socket.on('create or join', function (room) {
-      //     var numClients = that.io.sockets.clients(room).length;
-      //
-      //     log('Room ' + room + ' has ' + numClients + ' client(s)');
-      //     log('Request to create or join room ' + room);
-      //
-      //     if (numClients === 0){
-      //         socket.join(room);
-      //         socket.emit('created', room);
-      //     } else if (numClients === 1) {
-      //         that.io.sockets.in(room).emit('join', room);
-      //         socket.join(room);
-      //         socket.emit('joined', room);
-      //     } else { // max two clients
-      //         socket.emit('full', room);
-      //     }
-      //     socket.emit('emit(): client ' + socket.id + ' joined room ' + room);
-      //     socket.broadcast.emit('broadcast(): client ' + socket.id + ' joined room ' + room);
-      // });
     });
 }
