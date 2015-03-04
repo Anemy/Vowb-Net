cf91203091961a88e9add2101ba4d3200fc4e014
iyer8 <iyer8@purdue.edu>
2015-03-04 17:26:39 -0500

    Text modifications. Voice chat works on server

--- public/javascripts/locallobby.js
+++ public/javascripts/locallobby.js
@@ -44,36 +44,29 @@ connection.onstream = function(e) {
 
 var inSession = false;
 //sessions manages the current lobbies in the sub lobby.
-var sessions;
+var sessions = {};
 connection.onNewSession = function(session) {
 
     console.log("Session: " + session.extra['session-name'] + " was created.");
     inSession = true;
-    // if (madeSession) {
-    //     return;
-    // }
-    // if (sessions[session.sessionid]) {
-    //     return;
-    // }
-    sessions = session;
-    connection.join(sessions);
-    // var tr = document.createElement('tr');
-    // tr.innerHTML = '<td><strong>' + session.extra['session-name'] + '</strong> lobby</td>' +
-    //     '<td><button class="join">Join</button></td>';
-    // roomsList.insertBefore(tr, roomsList.firstChild);
-    // var joinRoomButton = tr.querySelector('.join');
-    // joinRoomButton.setAttribute('data-sessionid', session.sessionid);
-    // joinRoomButton.onclick = function() {
-    //     this.disabled = true;
-    //     var sessionid = this.getAttribute('data-sessionid');
-    //     session = sessions[sessionid];
-    //     if (!session) throw 'No such session exists.';
-    //     connection.join(session);
-    // };
-
+    if (sessions[session.sessionid]) return;
+        sessions[session.sessionid] = session;
+        var tr = document.createElement('tr');
+        tr.innerHTML = '<td><strong>' + session.extra['session-name'] + '</strong> is running a conference!</td>' +
+            '<td><button class="join">Join</button></td>';
+        roomsList.insertBefore(tr, roomsList.firstChild);
+        var joinRoomButton = tr.querySelector('.join');
+        joinRoomButton.setAttribute('data-sessionid', session.sessionid);
+        joinRoomButton.onclick = function() {
+            this.disabled = true;
+            var sessionid = this.getAttribute('data-sessionid');
+            session = sessions[sessionid];
+            if (!session) throw 'No such session exists.';
+            connection.join(session);
+    };
 };
 
-setTimeout(function() {
+/*setTimeout(function() {
     //this.disabled = true;
     if(inSession == false) {
         console.log("Couldn't find any sessions... We're creating one.");
@@ -83,16 +76,25 @@ setTimeout(function() {
         };
         connection.open();
     }
-}, 5000);
+}, 5000);*/
 
 var audioContainer = document.getElementById('audios-container') || document.body;
 var roomsList = document.getElementById('rooms-list');
-// document.getElementById('setup-new-conference').onclick = function() {
-//     this.disabled = true;
-//     connection.extra = {
-//         'session-name': document.getElementById('conference-name').value || 'Anonymous'
-//     };
-//     connection.open();
-// };
+document.getElementById('setup-new-conference').onclick = function() {
+    this.disabled = true;
+    connection.extra = {
+       'session-name': document.getElementById('conference-name').value || 'Anonymous'
+    };
+    connection.open();
+};
+
 // setup signaling to search existing sessions
 connection.connect();
+
+ (function() {
+            var uniqueToken = document.getElementById('unique-token');
+            if (uniqueToken)
+                if (location.hash.length > 2) uniqueToken.parentNode.parentNode.parentNode.innerHTML = '<h2 style="text-align:center;"><a href="' + location.href + '" target="_blank">Share this link</a></h2>';
+                else uniqueToken.innerHTML = uniqueToken.parentNode.parentNode.href = '#' + (Math.random() * new Date().getTime()).toString(36).toUpperCase().replace(/\./g, '-');
+        })();
+
