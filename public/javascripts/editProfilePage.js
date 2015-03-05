397fede4e200ad5fa95b695ad4142de03887c8e1
Rhys Howell <rhysh@live.com>
2015-03-05 13:43:35 -0500

    Started adding file picker io

--- public/javascripts/editProfilePage.js
+++ public/javascripts/editProfilePage.js
@@ -16,6 +16,18 @@ $('#editProfileForm').ready (function() {
         console.log ("editProfileForm form submitted");
         var errMsg = "";
         var err = 0;
-        
+
     });
+
+
+    //image picker code
+    // var filePicked = function(e) {
+    //   console.log(e.fpfile.url)
+    // };
+    //input(type='filepicker', data-fp-apikey='AAhXvilT1RIaTrxakvtWnz', data-fp-mimetypes='*/*', data-fp-container='modal', data-fp-services='COMPUTER,DROPBOX,GOOGLE_DRIVE,URL', onchange='filePicked(event)' data-fp-button-text="UPLOAD LOGO")
+    // filepicker.setKey("AxXSSg71vQROpwHyvp1Iaz");
+    // filepicker.pickAndStore({mimetype:"image/*"},{},
+    //   function(InkBlobs){
+    //     console.log(JSON.stringify(InkBlobs));
+    // });
 });
