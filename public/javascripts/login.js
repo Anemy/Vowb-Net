3d089aacf44db0c734bf00bb875ea59f4f6fe7ca
Pascal <lee1543@purdue.edu>
2015-03-05 23:45:51 -0500

    underline

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,9 +1,19 @@
 /* This script will open the log in dialogue and handle client side log in things */
 
+var loggedin = false;
+var user_name = "";
+
+
 var loginButtonClicked = function() {
-  $('.signupButton').addClass('dontShowGradient');
-  $('.loginPopup').fadeIn(50);
-  $('.overlay').fadeIn(50);
+  if (!loggedin) {
+    $('.signupButton').addClass('dontShowGradient');
+    $('.loginPopup').fadeIn(50);
+    $('.overlay').fadeIn(50);
+  } else {
+    var url = "/users/";
+    url = url.concat(user_name);
+    window.location.href = url;
+  }
 }
 var exitButtonClicked = function() {
 	$('.loginPopup').fadeOut(50);
@@ -23,9 +33,12 @@ var loginSubClicked = function() {
             },
             success: function(data){
                 console.log("Login success!!");
+                user_name = $("#un_id").val();
                 $("#signup_id").text("log out");
                 $("#login_id").text($("#un_id").val());
+                $("#login_id").css("text-decoration", "underline");
                 $('.loginPage').fadeOut(50);
+                loggedin = true;
             },
             error: function(data){
                 alert("Username or password is incorrect.");
