032c93b18d501a0e34dde304d95cf8751217e323
everetm <everetm@purdue.edu>
2015-03-31 18:03:18 -0400

    no logger see edit profile button on profiles that aren't yours

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -9,12 +9,22 @@ var editProfButtonClicked = function () {
 
 /* This javascript manages the sign up page's client side interactions */
 $(document).ready(function() {    
-      if(loginData != "none") {
-          console.log("Has a session! " + loginData);
-          showLoggedIn( loginData );
-      }
-      else {
-        console.log("No session :'(");
-      }
+    if(loginData != "none") {
+        console.log("Has a session! " + loginData);
+        showLoggedIn( loginData );
+    }
+    else {
+      console.log("No session :'(");
+    }
+    var checkurl = "/users/";
+    checkurl = checkurl.concat(user_name)
+    console.log(window.location.pathname);
+    console.log(checkurl);
+    if(window.location.pathname != checkurl) {
+      $('.editProfButton').hide();
+    }
+    else {
+      $('.editProfButton').show();
+    }
 
 });
